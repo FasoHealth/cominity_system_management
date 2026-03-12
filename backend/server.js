f0d5829190c3cb7fa0f5ab0cc58bc6881241adea
Stephie-fa <yanogofabianastephanie@gmail.com>
2026-03-12 23:56:37 +0000

    inscription

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,13 @@ const allowedOrigins = [
 ].filter(Boolean);
 
 app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
+    origin: [
+        'http://localhost:3000',
+        'https://security-allert.netlify.app'
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
